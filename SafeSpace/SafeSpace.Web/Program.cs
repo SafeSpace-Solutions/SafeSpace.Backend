239e3a9e18c360abd1cfcae6819ecf4a323ac4e3
tapasvi123 <tapasviamruth4@gmail.com>
2024-01-27 19:49:26 +0530

    Change made.

--- SafeSpace/SafeSpace.Web/Program.cs
+++ SafeSpace/SafeSpace.Web/Program.cs
@@ -31,6 +31,7 @@ namespace SafeSpace.Web
             builder.Services.AddSwaggerGen();
             builder.Services.AddScoped<JwtService>();
             builder.Services.AddScoped<EmailService>();
+
             builder.Services.AddDbContext<SafeSpaceDbContext>(options =>
             {
                 options.UseMySql(builder.Configuration.GetConnectionString("SafeSpaceDatabase"),
